ebafdc7a4a44195f56bee239d7e38db0c507fa2c
ASHMIX <adishram006@gmail.com>
2026-05-13 22:53:22 +0530

    Fix asset filenames in cask

--- Casks/ay.rb
+++ Casks/ay.rb
@@ -3,11 +3,11 @@ cask "ay" do
 
   on_intel do
     sha256 "bdc62bdb0d821040ee18f32d9ee52f33bab3fef2a7b287114924f753614f20e9"
-    url "https://github.com/Adishram/ay-messaging-app/releases/download/v#{version}/A%26Y-#{version}.dmg"
+    url "https://github.com/Adishram/ay-messaging-app/releases/download/v#{version}/A.Y-#{version}.dmg"
   end
   on_arm do
     sha256 "cd311da671ae3fa20597be99c39629942d1fe275785e96571a95c65e712885d0"
-    url "https://github.com/Adishram/ay-messaging-app/releases/download/v#{version}/A%26Y-#{version}-arm64.dmg"
+    url "https://github.com/Adishram/ay-messaging-app/releases/download/v#{version}/A.Y-#{version}-arm64.dmg"
   end
 
   name "A&Y"
